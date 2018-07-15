35c1ab8cc1003298431cd3bab1ae73662047fa26
Matoyak <mwilliam09@gmail.com>
2018-07-15 03:19:29 -0500

    Finishing naming adjustment

--- SDi_application/Models/ApplicationDbContext.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore;
-
-namespace SDi_application.Models {
-    public class ApplicationDbContext : DbContext {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
-        public DbSet<Student> Students { get; set; }
-    }
-}
--- SDi_application/src/app/Models/Student.ts
+++
@@ -1,8 +0,0 @@
-export class Student {
-  firstName: string;
-  lastName: string;
-  dateOfBirth: Date;
-  gpa: number;
-  id?: number;
-  userName?: string;
-}
